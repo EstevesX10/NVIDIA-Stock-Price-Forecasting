226ef61192e7ffe91470cd5ece060639fe79e228
Gongas <goncalo.mesteves03@gmail.com>
2024-08-13 17:12:46 +0100

    Added the Config file to the custom package

--- NVIDIA Stock Predictor/CustomUtilities/Configuration.py
+++ NVIDIA Stock Predictor/CustomUtilities/Configuration.py
@@ -4,5 +4,4 @@ def load_config() -> dict:
             'N':3, # Number of previous data to be considered when predicting a next value
             'max_period':False, # Flag to determine whether or not to consider all the historical data, i.e, all the information available 
             'start_date':'2022-12-04', # Start date of the data to be considered
-            'end_date':'2024-08-12' # End date of the data to be considered
-            }
+            'end_date':'2024-08-12'} # End date of the data to be considered
--- NVIDIA Stock Predictor/CustomUtilities/__init__.py
+++ NVIDIA Stock Predictor/CustomUtilities/__init__.py
@@ -1,6 +1,8 @@
 # Defining which submodules to import when using from <package> import *
-__all__ = ["str_to_datetime", "df_to_windowed_df", "windowed_df_to_date_X_y",
+__all__ = ["load_config",
+           "str_to_datetime", "df_to_windowed_df", "windowed_df_to_date_X_y",
            "plot_model_performance", "plot_model_overall_performance"]
 
+from .Configuration import (load_config)
 from .DataPreProcessing import (str_to_datetime, df_to_windowed_df, windowed_df_to_date_X_y)
 from .DataVisualization import (plot_model_performance, plot_model_overall_performance)
