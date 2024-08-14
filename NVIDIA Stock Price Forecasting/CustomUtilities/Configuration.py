b800bf7e37b936b9ff3a4419e60bee5b5d02b6b4
Gongas <goncalo.mesteves03@gmail.com>
2024-08-14 19:12:09 +0100

    Added an additional flag to the config that determines whether or not it is to save the final plot

--- NVIDIA Stock Price Forecasting/CustomUtilities/Configuration.py
+++ NVIDIA Stock Price Forecasting/CustomUtilities/Configuration.py
@@ -4,4 +4,5 @@ def load_config() -> dict:
             'N':3, # Number of previous data to be considered when predicting a next value
             'max_period':False, # Flag to determine whether or not to consider all the historical data, i.e, all the information available 
             'start_date':'2022-12-04', # Start date of the data to be considered
-            'end_date':'2024-08-12'} # End date of the data to be considered
+            'end_date':'2024-08-12', #  End date of the data to be considered
+            'save_plot':False} # Flag that decides whether or not to save the Final graph of the Notebook
